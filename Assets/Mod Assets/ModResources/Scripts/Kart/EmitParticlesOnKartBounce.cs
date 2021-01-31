c059a7f27b832ebcef8538f02d583e8af8785033
Juanktigre <juanktigre@gmail.com>
2021-01-31 11:48:28 -0500

    UI scroll edit look colors out of scroll fixed and timelimit mode fixed

--- Assets/Mod Assets/ModResources/Scripts/Kart/EmitParticlesOnKartBounce.cs
+++ Assets/Mod Assets/ModResources/Scripts/Kart/EmitParticlesOnKartBounce.cs
@@ -13,7 +13,7 @@ public class EmitParticlesOnKartBounce : MonoBehaviour
         effect = GetComponent<ParticleSystem>();
         vehicleBounce = GetComponentInParent<KartBounce>();
 
-        Assert.IsNotNull(vehicleBounce, "This particle should be a child of a VehicleBounce!");
+        // Assert.IsNotNull(vehicleBounce, "This particle should be a child of a VehicleBounce!");
     }
 
     void Update()
